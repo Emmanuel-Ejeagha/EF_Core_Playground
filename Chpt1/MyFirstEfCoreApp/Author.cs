31e5b727823e0ee6c0d8bb31862cf3e1a743b392
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-02 12:09:49 +0100

    Add Author properties

---
+++ Chpt1/MyFirstEfCoreApp/Author.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyFirstEfCoreApp;
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+    public string? WebUrl { get; set; }
+}
