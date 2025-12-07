8006c191b2e235cf641f77a144a3cfe20cd0554b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-07 14:55:15 +0100

    Add product property and constructor

---
+++ Asp.NetCoreInAction/A-Routing/Product.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace A_Routing;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
