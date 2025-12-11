5149a1aaf6171986355f64acf39e184314ab2c52
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-11 01:21:39 +0100

    Add list of ingridents

---
+++ Asp.NetCoreInAction/InstallEFCore/Commands/CreateRecipeCommand.cs
@@ -0,0 +1,22 @@
+using System;
+using InstallEFCore.Models;
+
+namespace InstallEFCore.Commands;
+
+public class CreateRecipeCommand : EditRecipeBase
+{
+    public IList<CreateIngredientCommand> Ingredients { get; set; } = new List<CreateIngredientCommand>();
+
+    public Recipe ToRecipe()
+    {
+        return new Recipe
+        {
+            Name = Name,
+            TimeToCook = new TimeSpan(TimeToCookHrs, TimeToCookMins, 0),
+            Method = Method,
+            IsVegetarian = IsVegetarian,
+            IsVegan = IsVegan,
+            Ingredients = Ingredients.Select(x => x.ToIngredient()).ToList()
+        };
+    }
+}
