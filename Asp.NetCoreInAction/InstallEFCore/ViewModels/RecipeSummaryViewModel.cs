d92f509adadbbfc3a3ddcf409d30984e5b10e022
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-11 01:32:53 +0100

    Add Recipe summary View model

---
+++ Asp.NetCoreInAction/InstallEFCore/ViewModels/RecipeSummaryViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+using InstallEFCore.Models;
+
+namespace InstallEFCore.ViewModels;
+
+public class RecipeSummaryViewModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string TimeToCook { get; set; }
+    public int NumberOfIngredients { get; set; }
+
+    public static RecipeSummaryViewModel FromRecipe(Recipe recipe) => new RecipeSummaryViewModel
+    {
+        Id = recipe.RecipeId,
+        Name = recipe.Name,
+        TimeToCook = $"{recipe.TimeToCook.Hours}hrs {recipe.TimeToCook.Minutes}mins"
+    };
+}
