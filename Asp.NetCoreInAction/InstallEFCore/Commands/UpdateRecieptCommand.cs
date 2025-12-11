080c421fd525b5e8722b5b6fe3d8f64044a7925a
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-11 01:25:20 +0100

    update recipe

---
+++ Asp.NetCoreInAction/InstallEFCore/Commands/UpdateRecieptCommand.cs
@@ -0,0 +1,16 @@
+using System;
+using InstallEFCore.Models;
+
+namespace InstallEFCore.Commands;
+
+public class UpdateRecipeCommand : EditRecipeBase
+{
+    public int Id { get; set; }
+    public void UpdateRecipe(Recipe recipe)
+    {
+        recipe.Name = Name;
+        recipe.TimeToCook = new TimeSpan(TimeToCookHrs, TimeToCookMins, 0);
+        recipe.IsVegetarian = IsVegetarian;
+        recipe.IsVegan  = IsVegan;
+    }
+}
