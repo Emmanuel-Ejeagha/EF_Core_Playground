04abf53c29ca410dbaeeee45c2a8793148b764b5
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-15 20:24:18 +0100

    Add ToDo Model

---
+++ Asp.NetCoreInAction/ToDoList/ToDoModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToDoList;
+
+public class ToDoModel
+{
+    public int Number { get; set; }
+    public string Title { get; set; }
+    public string Login { get; set; }
+    public string State { get; set; }
+}
