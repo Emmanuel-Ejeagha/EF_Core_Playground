67edac5eb8e9fd68196eb89c198e1bd7f4327e4d
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-15 20:26:06 +0100

    ToDo List

---
+++ Asp.NetCoreInAction/ToDoList/Pages/ToDo/ListCategory.cshtml.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoList.Pages.ToDo
+{
+    public class ListCategoryModel : PageModel
+    {
+        private readonly ToDoService _service;
+
+        public ListCategoryModel(ToDoService service)
+        {
+            _service = service;
+        }
+        [BindProperty(SupportsGet = true)]
+        public InputModel Input { get; set; }
+        public IEnumerable<Task> Tasks { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+                // TODO: Validate the parameters
+                Tasks = _service.GetToDoItems(Input.Category, Input.Username)
+                    .Select(x => new Task(x.Number, x.Title));
+                return Page();
+            
+        }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            public string Category { get; set; }
+        }
+
+        public class Task
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public Task(int id, string description)
+            {
+                Id = id;
+                Description = description;
+            }
+        }
+    }
+}
