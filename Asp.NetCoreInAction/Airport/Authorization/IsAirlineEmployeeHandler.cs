31bd03e7a4c458c355bbe9ad2be01c9623471f85
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-24 16:42:30 +0100

    Add IsAirline Employee Handler

---
+++ Asp.NetCoreInAction/Airport/Authorization/IsAirlineEmployeeHandler.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Airport.Authorization;
+
+public class IsAirlineEmployeeHandler : AuthorizationHandler<AllowedInLoungeRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AllowedInLoungeRequirement requirement)
+    {
+        if (context.User.HasClaim(c => c.Type == Claims.EmployeeNumber))
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
