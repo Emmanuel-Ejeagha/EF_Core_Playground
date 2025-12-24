12ea1ba1bae6c1fa657ca961894ccb239362e18a
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-24 16:35:10 +0100

    Add Airport page

---
+++ Asp.NetCoreInAction/Airport/Pages/Airport/AirportLounge.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Airport.Pages.Airport
+{
+    public class AirportLoungeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
