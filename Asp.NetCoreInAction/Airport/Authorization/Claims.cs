8c37133a1ddd654ff3cd04e2a4ad8c57aec2aa48
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-24 16:39:51 +0100

    Add Claims

---
+++ Asp.NetCoreInAction/Airport/Authorization/Claims.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Airport.Authorization;
+
+public class Claims
+{
+    public const string IsBannedFromLounge = "IsBannedFromLounge";
+    public const string FrequentFlyerClass = "FrequentFlyerClass";
+    public const string EmployeeNumber = "EmployeeNumber";
+    public const string BoardingPassNumber = "BoardPassNumber";
+}
