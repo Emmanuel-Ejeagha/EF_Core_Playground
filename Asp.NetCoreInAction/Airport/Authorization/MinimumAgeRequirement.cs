b1d7baecf0ba0c2911189eb711d06a967fc3bad2
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-12-24 16:43:18 +0100

    Add Mininum Age Requirement

---
+++ Asp.NetCoreInAction/Airport/Authorization/MinimumAgeRequirement.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Airport.Authorization;
+
+public class MinimumAgeRequirement :IAuthorizationRequirement
+{
+    public MinimumAgeRequirement(int minimumAge)
+    {
+        MinimumAge = minimumAge;
+    }
+
+    public int MinimumAge { get;}
+}
